29e506cf63aaf3bfc613988e2a8148b6902fe820
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-30 18:22:41 -0700

    "ads2_m2_a1_1: 18/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m2/2.1 CC_Percolation/Bag.java
+++ Assignments/m2/2.1 CC_Percolation/Bag.java
@@ -1,136 +1,82 @@
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-/**
- * Class for bag.
- *
- * @param      <Item>  The item
- */
+
 public class Bag<Item> implements Iterable<Item> {
-    /**
-     * { var_description }.
-     */
-    private int size;
-    /**
-     * { var_description }.
-     */
-    private Node first;
-    /**
-     * Class for node.
-     */
-    private class Node {
-        /**
-         * { var_description }.
-         */
+    private Node<Item> first;    // beginning of bag
+    private int n;               // number of elements in bag
+
+    // helper linked list class
+    private static class Node<Item> {
         private Item item;
-        /**
-         * { var_description }.
-         */
-        private Node next;
+        private Node<Item> next;
     }
 
     /**
-      * Create an empty stack.
-      */
+     * Initializes an empty bag.
+     */
     public Bag() {
         first = null;
-        size = 0;
+        n = 0;
     }
 
     /**
-      * Is the BAG empty.
-      */
-    /**
-     * Determines if empty.
+     * Returns true if this bag is empty.
      *
-     * @return     True if empty, False otherwise.
+     * @return {@code true} if this bag is empty;
+     *         {@code false} otherwise
      */
     public boolean isEmpty() {
         return first == null;
     }
 
     /**
-      * Return the number of items in the bag.
-      */
-    /**
-     * { function_description }.
+     * Returns the number of items in this bag.
      *
-     * @return     { description_of_the_return_value }
+     * @return the number of items in this bag
      */
     public int size() {
-        return size;
+        return n;
     }
 
     /**
-      * Add the item to the bag.
-      */
-    /**
-     * { function_description }.
+     * Adds the item to this bag.
      *
-     * @param      item  The item
+     * @param  item the item to add to this bag
      */
-    public void add(final Item item) {
-        Node oldfirst = first;
-        first = new Node();
+    public void add(Item item) {
+        Node<Item> oldfirst = first;
+        first = new Node<Item>();
         first.item = item;
         first.next = oldfirst;
-        size++;
+        n++;
     }
 
 
     /**
-      * Return an iterator that iterates over the items in the bag.
-      */
-    /**
-     * { function_description }.
+     * Returns an iterator that iterates over the items in this bag in arbitrary order.
      *
-     * @return iterator.
+     * @return an iterator that iterates over the items in this bag in arbitrary order
      */
-    public Iterator<Item> iterator() {
-        return new ListIterator();
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);
     }
-    /**
-     * Class for list iterator.
-     */
-    private class ListIterator implements Iterator<Item> {
-        /**
-         * { var_description }.
-       */
-        private Node current = first;
-        /**
-         * Determines if it has next.
-         *
-         * @return     True if has next, False otherwise.
-         */
-        public boolean hasNext() {
-            return current != null;
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
         }
-        /**
-         * { function_description }.
-         */
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-        /**
-         * { function_description }.
-         *
-         * @return     { description_of_the_return_value }
-         */
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
         public Item next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
+            if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
             current = current.next;
             return item;
         }
     }
 }
-
