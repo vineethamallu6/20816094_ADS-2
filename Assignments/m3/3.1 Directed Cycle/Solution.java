f00235d292f9ffc38426b05e0081839c4eb5677a
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-31 21:48:24 -0700

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m3/3.1 Directed Cycle/Solution.java
+++ Assignments/m3/3.1 Directed Cycle/Solution.java
@@ -2,34 +2,34 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		//check style purpose.
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		DiGraph d = new DiGraph(vertices);
-		for (int i = 0; i < edges; i++) {
-			String[] tokens = scan.nextLine().split(" ");
-			int n1 = Integer.parseInt(tokens[0]);
-			int n2 = Integer.parseInt(tokens[1]);
-			d.addEdge(n1, n2);
-		}
-		DirectedCycle dc = new DirectedCycle(d);
-		if (dc.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
+        //check style purpose.
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        DiGraph d = new DiGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+            int n1 = Integer.parseInt(tokens[0]);
+            int n2 = Integer.parseInt(tokens[1]);
+            d.addEdge(n1, n2);
+        }
+        DirectedCycle dc = new DirectedCycle(d);
+        if (dc.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
