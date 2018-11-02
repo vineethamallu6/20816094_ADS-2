007d82e485f87aa9fef39402a0e8ff95032def91
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-02 16:46:42 -0700

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m4/WordNet/Solution.java
+++ Assignments/m4/WordNet/Solution.java
@@ -5,10 +5,10 @@ class Solution {
 	}
 	public static void main(String[] args) {
 		//Scanner scan = new Scanner(System.in);
-		String synFile = StdIn.readString();
-		String hyperFile = StdIn.readString();
+		String synFile = StdIn.readLine();
+		String hyperFile = StdIn.readLine();
 		//WordNet wn = new WordNet(synFile, hyperFile);
-		String token = StdIn.readString();
+		String token = StdIn.readLine();
 		try {
 			if (token.equals("Graph")) {
 				WordNet wn = new WordNet(synFile, hyperFile);
@@ -18,20 +18,18 @@ class Solution {
 			System.out.println(e.getMessage());
 		}
 		try {
-		if (token.equals("Queries")) {
-			WordNet wnq = new WordNet(synFile, hyperFile);
-			while (StdIn.hasNextLine()) {
-			String[] array = StdIn.readString().split(" ");
-			if (array[0].equals("null")) {
-				System.out.println("IllegalArgumentException");
+			if (token.equals("Queries")) {
+				WordNet wnq = new WordNet(synFile, hyperFile);
+				while (StdIn.hasNextLine()) {
+					String[] array = StdIn.readLine().split(" ");
+					if (array[0].equals("null")) {
+						throw new IllegalArgumentException("IllegalArgumentException");
+					}
+					System.out.println("distane = " + wnq.distance(array[0], array[1]) + ", ancestor = " + wnq.sap(array[0], array[1]));
+				}
 			}
-			System.out.println("distane = " + wnq.distance(array[0], array[1]) + ", ancestor = " + wnq.sap(array[0], array[1]));
-		}
+		} catch (Exception e) {
+		                System.out.println(e.getMessage());
+	    }
 	}
-} catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
 }
-
-
-	}
