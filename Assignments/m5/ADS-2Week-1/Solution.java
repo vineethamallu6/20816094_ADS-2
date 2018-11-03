ef898accc1b9fa5942a138fb77547ede833a31a8
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-03 13:23:40 -0700

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m5/ADS-2Week-1/Solution.java
+++ Assignments/m5/ADS-2Week-1/Solution.java
@@ -1,10 +1,54 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 class PageRank {
-
+	private Digraph dg;
+	private LinearProbingHashST<Integer, Double>  lb;
+	private int size = 0;
+	PageRank(Digraph d) {
+		this.dg = d;
+		lb = new LinearProbingHashST<Integer, Double>();
+	}
+	public void PageRank() {
+        // double prevpr = 0.25;
+        for (int i = 0; i < dg.V(); i++) {
+            lb.put(i, 0.25);
+        }
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < dg.V(); j++) {
+                double temp = 0.0;
+                double temp1 = 0.0;
+                for (int k : dg.adj(j)) {
+                    //int cnt = 0;
+                    if (k == i) {
+                        temp = lb.get(k) / dg.outdegree(k) + 1;
+                        temp1 = temp1 + temp;
+                    } else {
+                        temp = lb.get(j) / dg.outdegree(j);
+                        temp1 = temp1 + temp;
+                    }
+                    //System.out.println(finaltemp);
+                }
+                lb.put(j, temp1);
+            }
+        }
+    }
+    /**.
+     * { function_description }
+     */
+    public void string() {
+        for (int i : lb.keys()) {
+            String s = "";
+            s += String.valueOf(i) + " - " + lb.get(i);
+            System.out.println(s);
+        }
+    }
 }
 
+
 class WebSearch {
+	WebSearch() {
+
+	}
 
 }
 
@@ -18,6 +62,7 @@ public class Solution {
 		int n = Integer.parseInt(scan.nextLine());
 		int vertices = n;
 		Digraph d = new Digraph(n);
+
 		for (int i = 0; i < n; i++){
 			String[] tokens = scan.nextLine().split(" ");
 			int id = Integer.parseInt(tokens[0]);
@@ -25,14 +70,18 @@ public class Solution {
 				d.addEdge(id, Integer.parseInt(tokens[j]));
 			}
 		}
-		System.out.println(d.V() + " vertices, " + d.E() + " edges ");
-		for (int i = 0; i < vertices; i++) {
-			System.out.print(i + ": ");
-			for (int j : d.adj(i)) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
+		System.out.println(d.toString());
+		// System.out.println(d.V() + " vertices, " + d.E() + " edges ");
+		// for (int i = 0; i < vertices; i++) {
+		// 	System.out.print(i + ": ");
+		// 	for (int j : d.adj(i)) {
+		// 		System.out.print(j + " ");
+		// 	}
+		// 	System.out.println();
+		// }
+		PageRank p = new PageRank(d);
+		p.PageRank();
+	p.string();
 		// read the first line of the input to get the number of vertices
 
 		// iterate count of vertices times
