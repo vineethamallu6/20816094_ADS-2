b9ef2b0e86da47c0316b63f76515745221cf81fb
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-05 19:39:46 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m7/1. MST/Solution.java
+++ Assignments/m7/1. MST/Solution.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
+/**
+ * { item_description }
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	Solution() {
 		//ckeck style purpose.
 	}
-	public static void main(String[] args) {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
 		EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
 		while (scan.hasNext()) {
 			String[] array = scan.nextLine().split(" ");
-			Edge ed = new Edge(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Double.parseDouble(array[2]));
+			Edge ed = new Edge(Integer.parseInt(array[0]),
+				Integer.parseInt(array[1]),
+				Double.parseDouble(array[2]));
 			ewg.addEdge(ed);
 		}
 		LazyPrimMST mst = new LazyPrimMST(ewg);
