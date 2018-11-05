08dbbd9b8ead49e0f893903cdbf1b765cfa92651
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-04 21:02:40 -0800

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m4/WordNet/Digraph.java
+++ Assignments/m4/WordNet/Digraph.java
@@ -1,33 +1,3 @@
-/******************************************************************************
- *  Compilation:  javac Digraph.java
- *  Execution:    java Digraph filename.txt
- *  Dependencies: Bag.java In.java StdOut.java
- *  Data files:   https://algs4.cs.princeton.edu/42digraph/tinyDG.txt
- *                https://algs4.cs.princeton.edu/42digraph/mediumDG.txt
- *                https://algs4.cs.princeton.edu/42digraph/largeDG.txt
- *
- *  A graph, implemented using an array of lists.
- *  Parallel edges and self-loops are permitted.
- *
- *  % java Digraph tinyDG.txt
- *  13 vertices, 22 edges
- *  0: 5 1
- *  1:
- *  2: 0 3
- *  3: 5 2
- *  4: 3 2
- *  5: 4
- *  6: 9 4 8 0
- *  7: 6 9
- *  8: 6
- *  9: 11 10
- *  10: 12
- *  11: 4 12
- *  12: 9
- *
- ******************************************************************************/
-
-
 import java.util.NoSuchElementException;
 
 /**
--- Assignments/m4/WordNet/SAP.java
+++ Assignments/m4/WordNet/SAP.java
@@ -1,15 +1,47 @@
+/*public class SAP {
+
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G)
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w)
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w)
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w)
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
+
+    // do unit testing of this class
+    public static void main(String[] args)
+}*/
 public class SAP {
     private Digraph dg;
     private BreadthFirstDirectedPaths[] bfs;
 
     // constructor takes a digraph (not necessarily a DAG)
-    public SAP(final Digraph G) {
+    public SAP(Digraph G) {
         this.dg = new Digraph(G);
         bfs = new BreadthFirstDirectedPaths[this.dg.V()];
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(final int v, final int w) {
+        if (v < 0 || v > dg.V() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (w < 0 || w > dg.V() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (bfs[v] == null) {
+            bfs[v] = new BreadthFirstDirectedPaths(dg, v);
+        }
+        if (bfs[w] == null) {
+            bfs[w] = new BreadthFirstDirectedPaths(dg, w);
+        }
         int length = Integer.MAX_VALUE;
         for (int i = 0; i < dg.V(); i++) {
             if (bfs[v].hasPathTo(i) && bfs[w].hasPathTo(i)) {
@@ -19,6 +51,8 @@ public class SAP {
                 }
             }
         }
+        // bfs[v] = null;
+        // bfs[w] = null;
         if (length != Integer.MAX_VALUE) {
             return length;
         } else {
@@ -29,7 +63,23 @@ public class SAP {
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(final int v, final int w) {
+    public int ancestor(int v, int w) {
+        if (v < 0 || v > dg.V() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (w < 0 || w > dg.V() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (bfs[v] == null) {
+            bfs[v] = new BreadthFirstDirectedPaths(dg, v);
+        }
+
+        if (bfs[w] == null) {
+            bfs[w] = new BreadthFirstDirectedPaths(dg, w);
+        }
+
         int length = Integer.MAX_VALUE;
         int ancestor = -1;
         for (int i = 0; i < dg.V(); i++) {
@@ -41,12 +91,17 @@ public class SAP {
                 }
             }
         }
+        // bfs[v] = null;
+        // bfs[w] = null;
         return ancestor;
 
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(final Iterable<Integer> v, final Iterable<Integer> w) {
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null || w == null) {
+            throw new NullPointerException();
+        }
         int length = Integer.MAX_VALUE;
         for (int i : v) {
             for (int j : w) {
@@ -64,7 +119,11 @@ public class SAP {
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(final Iterable<Integer> v, final Iterable<Integer> w) {
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null || w == null) {
+            throw new NullPointerException();
+        }
+
         int length = Integer.MAX_VALUE;
         int ancestor = -1;
 
@@ -77,7 +136,11 @@ public class SAP {
                 }
             }
         }
-
+        //assert length != -1;
         return ancestor;
     }
-}
+
+    }
+
+    // do unit testing of this class
+   // public static void main(String[] args)
