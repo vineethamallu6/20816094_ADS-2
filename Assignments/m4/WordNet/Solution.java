d480a19f1e10ecb664eaf88abbe69845cea3912b
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-04 21:10:50 -0800

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m4/WordNet/Solution.java
+++ Assignments/m4/WordNet/Solution.java
@@ -1,47 +1,50 @@
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
 
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		//Scanner scan = new Scanner(System.in);
-		String synFile = StdIn.readLine();
-		String hyperFile = StdIn.readLine();
-		//WordNet wn = new WordNet(synFile, hyperFile);
-		String token = StdIn.readLine();
-		try {
-			if (token.equals("Graph")) {
-				WordNet wn = new WordNet(synFile, hyperFile);
-				wn.display();
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			if (token.equals("Queries")) {
-				WordNet wnq = new WordNet(synFile, hyperFile);
-				while (StdIn.hasNextLine()) {
-					String[] array = StdIn.readLine().split(" ");
-					if (array[0].equals("null")) {
-						throw new IllegalArgumentException(
-						"IllegalArgumentException");
-					}
-					System.out.println("distance = " + wnq.distance(array[0], array[1])
-					+ ", ancestor = " + wnq.sap(array[0], array[1]));
-				}
-			}
-		} catch (Exception e) {
-		    System.out.println(e.getMessage());
-	    }
-	}
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        //Scanner scan = new Scanner(System.in);
+        String synFile = StdIn.readLine();
+        String hyperFile = StdIn.readLine();
+        //WordNet wn = new WordNet(synFile, hyperFile);
+        String token = StdIn.readLine();
+        try {
+            if (token.equals("Graph")) {
+                WordNet wn = new WordNet(synFile, hyperFile);
+                wn.display();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            if (token.equals("Queries")) {
+                WordNet wnq = new WordNet(synFile, hyperFile);
+                while (StdIn.hasNextLine()) {
+                    String[] array = StdIn.readLine().
+                    split(" ");
+                    if (array[0].equals("null")) {
+                    throw new IllegalArgumentException(
+                    "IllegalArgumentException");
+                    }
+                    System.out.println("distance = "
+                    + wnq.distance(array[0], array[1])
+                    + ", ancestor = " + wnq.sap(array[0],
+                    array[1]));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
