3345a14a7909ec7df46dfad064353d583a748a53
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-08 13:39:57 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m10/Seam Carving/SeamCarver.java
+++ Assignments/m10/Seam Carving/SeamCarver.java
@@ -1,4 +1,4 @@
-
+import java.awt.Color;
 public class SeamCarver {
 	// create a seam carver object based on the given picture
 	private Picture picture;
@@ -24,7 +24,27 @@ public class SeamCarver {
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		return 0;
+		if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1)
+            return 1000;
+        Color top = picture.get(x, y - 1);
+        Color bottom = picture.get(x, y + 1);
+        Color left = picture.get(x - 1, y);
+        Color right = picture.get(x + 1, y);
+        int r = right.getRed() - left.getRed();
+        int b = right.getBlue() - left.getBlue();
+        int g = right.getGreen() - left.getGreen();
+        int ver_red = top.getRed() - bottom.getRed();
+        int ver_blue = top.getBlue() - bottom.getBlue();
+        int ver_green = top.getGreen() - bottom.getGreen();
+        int horizontal = r * r + b * b  +  g * g;
+        int vertical = ver_red * ver_red + ver_blue * ver_blue + ver_green * ver_green;
+        double energy = Math.sqrt(horizontal + vertical);
+        return energy;
+		// int right = picture.getRGB(x + 1, y);
+		// int left = picture.getRGB(x-1, y);
+		// int top = picture.getRGB(x, y-1);
+		// int dowm = picture.getRGB(x, y+1);
+
 	}
 
 	// sequence of indices for horizontal seam
