fc80c96e9a66476aff47eb5899a60140055c0de2
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-11-13 21:37:49 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m14/Assignment-1/SuffixArray.java
+++ Assignments/m14/Assignment-1/SuffixArray.java
@@ -1,68 +1,50 @@
 import java.util.Arrays;
-
 /**
- *  The {@code SuffixArray} class represents a suffix array of a string of
- *  length <em>n</em>.
- *  It supports the <em>selecting</em> the <em>i</em>th smallest suffix,
- *  getting the <em>index</em> of the <em>i</em>th smallest suffix,
- *  computing the length of the <em>longest common prefix</em> between the
- *  <em>i</em>th smallest suffix and the <em>i</em>-1st smallest suffix,
- *  and determining the <em>rank</em> of a query string (which is the number
- *  of suffixes strictly less than the query string).
- *  <p>
- *  This implementation uses a nested class {@code Suffix} to represent
- *  a suffix of a string (using constant time and space) and
- *  {@code Arrays.sort()} to sort the array of suffixes.
- *  The <em>index</em> and <em>length</em> operations takes constant time
- *  in the worst case. The <em>lcp</em> operation takes time proportional to the
- *  length of the longest common prefix.
- *  The <em>select</em> operation takes time proportional
- *  to the length of the suffix and should be used primarily for debugging.
- *  <p>
- *  For alternate implementations of the same API, see
- *  {@link SuffixArrayX}, which is faster in practice (uses 3-way radix quicksort)
- *  and uses less memory (does not create {@code Suffix} objects)
- *  and <a href = "https://algs4.cs.princeton.edu/63suffix/SuffixArrayJava6.java.html">SuffixArrayJava6.java</a>,
- *  which relies on the constant-time substring extraction method that existed
- *  in Java 6.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/63suffix">Section 6.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for suffix array.
  */
-public class SuffixArray {
+class SuffixArray {
+    /**
+     * { var_description }.
+     */
     private Suffix[] suffixes;
-
     /**
      * Initializes a suffix array for the given {@code text} string.
-     * @param text the input string
+     *
+     * @param      textt  the input string
      */
-    public SuffixArray(final String text) {
-        int n = text.length();
+    SuffixArray(final String textt) {
+        int n = textt.length();
         this.suffixes = new Suffix[n];
-        for (int i = 0; i < n; i++)
-            suffixes[i] = new Suffix(text, i);
+        for (int i = 0; i < n; i++) {
+            suffixes[i] = new Suffix(textt, i);
+        }
         Arrays.sort(suffixes);
     }
+
     /**
      * Class for suffix.
      */
-
-    private static class Suffix implements Comparable<Suffix> {
+    private static final class Suffix implements Comparable<Suffix> {
+        /**
+         * { var_description }.
+         */
         private final String text;
+        /**
+         * { var_description }.
+         */
         private final int index;
         /**
          * Constructs the object.
          *
-         * @param      text   The text
-         * @param      index  The index
+         * @param      textt   The text
+         * @param      indexx  The index
          */
-
-        private Suffix(final String text, final int index) {
-            this.text = text;
-            this.index = index;
+        private Suffix(final String textt, final int indexx) {
+            this.text = textt;
+            this.index = indexx;
         }
         /**
-         * { function_description }
+         * { function_description }.
          *
          * @return     { description_of_the_return_value }
          */
@@ -86,13 +68,18 @@ public class SuffixArray {
          *
          * @return     { description_of_the_return_value }
          */
-
         public int compareTo(final Suffix that) {
-            if (this == that) return 0;  // optimization
+            if (this == that) {
+                return 0;
+            }
             int n = Math.min(this.length(), that.length());
             for (int i = 0; i < n; i++) {
-                if (this.charAt(i) < that.charAt(i)) return -1;
-                if (this.charAt(i) > that.charAt(i)) return +1;
+                if (this.charAt(i) < that.charAt(i)) {
+                    return -1;
+                }
+                if (this.charAt(i) > that.charAt(i)) {
+                    return +1;
+                }
             }
             return this.length() - that.length();
         }
@@ -101,30 +88,33 @@ public class SuffixArray {
          *
          * @return     String representation of the object.
          */
-
         public String toString() {
             return text.substring(index);
         }
     }
-
     /**
      * Returns the length of the input string.
-     * @return the length of the input string
+     *
+     * @return     the length of the input string
      */
     public int length() {
         return suffixes.length;
     }
-
-
     /**
-     * Returns the index into the original string of the <em>i</em>th smallest suffix.
-     * That is, {@code text.substring(sa.index(i))} is the <em>i</em>th smallest suffix.
-     * @param i an integer between 0 and <em>n</em>-1
-     * @return the index into the original string of the <em>i</em>th smallest suffix
-     * @throws java.lang.IllegalArgumentException unless {@code 0 <= i < n}
+     * Returns the index into the original string of the <em>i</em>th smallest
+     * suffix. That is, {@code text.substring(sa.index(i))} is the <em>i</em>th
+     * smallest suffix.
+     *
+     * @param      i     an integer between 0 and <em>n</em>-1
+     *
+     * @return     the index into the original string of the <em>i</em>th
+     *             smallest suffix
+     * @throws     java.lang.IllegalArgumentException  unless {@code 0 <= i < n}
      */
     public int index(final int i) {
-        if (i < 0 || i >= suffixes.length) throw new IllegalArgumentException();
+        if (i < 0 || i >= suffixes.length) {
+            throw new IllegalArgumentException();
+        }
         return suffixes[i].index;
     }
 
@@ -132,14 +122,18 @@ public class SuffixArray {
     /**
      * Returns the length of the longest common prefix of the <em>i</em>th
      * smallest suffix and the <em>i</em>-1st smallest suffix.
-     * @param i an integer between 1 and <em>n</em>-1
-     * @return the length of the longest common prefix of the <em>i</em>th
-     * smallest suffix and the <em>i</em>-1st smallest suffix.
-     * @throws java.lang.IllegalArgumentException unless {@code 1 <= i < n}
+     *
+     * @param      i     an integer between 1 and <em>n</em>-1
+     *
+     * @return     the length of the longest common prefix of the <em>i</em>th
+     *             smallest suffix and the <em>i</em>-1st smallest suffix.
+     * @throws     java.lang.IllegalArgumentException  unless {@code 1 <= i < n}
      */
     public int lcp(final int i) {
-        if (i < 1 || i >= suffixes.length) throw new IllegalArgumentException();
-        return lcpSuffix(suffixes[i], suffixes[i-1]);
+        if (i < 1 || i >= suffixes.length) {
+            throw new IllegalArgumentException();
+        }
+        return lcpSuffix(suffixes[i], suffixes[i - 1]);
     }
 
     /**
@@ -153,41 +147,53 @@ public class SuffixArray {
     private static int lcpSuffix(final Suffix s, final Suffix t) {
         int n = Math.min(s.length(), t.length());
         for (int i = 0; i < n; i++) {
-            if (s.charAt(i) != t.charAt(i)) return i;
+            if (s.charAt(i) != t.charAt(i)) {
+                return i;
+            }
         }
         return n;
     }
 
     /**
      * Returns the <em>i</em>th smallest suffix as a string.
-     * @param i the index
-     * @return the <em>i</em> smallest suffix as a string
-     * @throws java.lang.IllegalArgumentException unless {@code 0 <= i < n}
+     *
+     * @param      i     the index
+     *
+     * @return     the <em>i</em> smallest suffix as a string
+     * @throws     java.lang.IllegalArgumentException
+     * unless {@code 0 <= i < n}
      */
     public String select(final int i) {
-        if (i < 0 || i >= suffixes.length) throw new IllegalArgumentException();
+        if (i < 0 || i >= suffixes.length) {
+            throw new IllegalArgumentException();
+        }
         return suffixes[i].toString();
     }
-
     /**
-     * Returns the number of suffixes strictly less than the {@code query} string.
-     * We note that {@code rank(select(i))} equals {@code i} for each {@code i}
-     * between 0 and <em>n</em>-1.
-     * @param query the query string
-     * @return the number of suffixes strictly less than {@code query}
+     * Returns the number of suffixes strictly less than the {@code query}
+     * string. We note that {@code rank(select(i))} equals {@code i} for each
+     * {@code i} between 0 and <em>n</em>-1.
+     *
+     * @param      query  the query string
+     *
+     * @return     the number of suffixes strictly less than {@code query}
      */
     public int rank(final String query) {
-        int lo = 0, hi = suffixes.length - 1;
+        int lo = 0;
+        int hi = suffixes.length - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             int cmp = compare(query, suffixes[mid]);
-            if (cmp < 0) hi = mid - 1;
-            else if (cmp > 0) lo = mid + 1;
-            else return mid;
+            if (cmp < 0) {
+                hi = mid - 1;
+            } else if (cmp > 0) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
         }
         return lo;
     }
-
     /**
      * { function_description }.
      *
@@ -199,8 +205,12 @@ public class SuffixArray {
     private static int compare(final String query, final Suffix suffix) {
         int n = Math.min(query.length(), suffix.length());
         for (int i = 0; i < n; i++) {
-            if (query.charAt(i) < suffix.charAt(i)) return -1;
-            if (query.charAt(i) > suffix.charAt(i)) return +1;
+            if (query.charAt(i) < suffix.charAt(i)) {
+                return -1;
+            }
+            if (query.charAt(i) > suffix.charAt(i)) {
+                return +1;
+            }
         }
         return query.length() - suffix.length();
     }
--- Assignments/m14/Assignment-1/TST.java
+++ Assignments/m14/Assignment-1/TST.java
@@ -1,18 +1,22 @@
-public class TST<Value> {
+/**
+ * Class for tst.
+ *
+ * @param      <Value>  The value
+ */
+class TST<Value> {
     /**
      * { var_description }.
      */
     private int n;
     /**
-     * { item_description }.
+     * { var_description }.
      */
     private Node<Value> root;
     /**
      * Class for node.
      *
-     * @param      <Value>  The value.
+     * @param      <Value>  The value
      */
-
     private static class Node<Value> {
         /**
          * { var_description }.
@@ -23,20 +27,22 @@ public class TST<Value> {
          */
         private Node<Value> left, mid, right;
         /**
-         * val.
+         * { var_description }.
          */
         private Value val;
     }
-
     /**
      * Initializes an empty string symbol table.
      */
-    public TST() {
+    TST() {
     }
 
     /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table.
+     * Returns the number of key-value pairs in
+     * this symbol table.
+     *
+     * @return     the number of key-value pairs
+     * in this symbol table
      */
     public int size() {
         return n;
@@ -44,123 +50,171 @@ public class TST<Value> {
 
     /**
      * Does this symbol table contain the given key?
-     * @param key the key
-     * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     *
+     * @param      key   the key
+     *
+     * @return     {@code true} if this symbol
+     * table contains {@code key} and
+     *             {@code false} otherwise
+     * @throws     IllegalArgumentException
+     * if {@code key} is {@code null}
      */
     public boolean contains(final String key) {
         if (key == null) {
-            throw new IllegalArgumentException
-            ("argument to contains() is null");
+            throw new IllegalArgumentException(
+                "argument to contains() is null");
         }
         return get(key) != null;
     }
 
     /**
      * Returns the value associated with the given key.
-     * @param key the key
-     * @return the value associated with the given key.
-     * if the key is in the symbol table.
-     *     and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}.
+     *
+     * @param      key   the key
+     *
+     * @return     the value associated with the
+     * given key if the key is in the
+     *             symbol table and {@code null}
+     *             if the key is not in the symbol
+     *             table
+     * @throws     IllegalArgumentException
+     * if {@code key} is {@code null}
      */
     public Value get(final String key) {
         if (key == null) {
-            throw new IllegalArgumentException
-            ("calls get() with null argument");
+            throw new IllegalArgumentException(
+                "calls get() with null argument");
         }
-        if (key.length() == 0) throw new IllegalArgumentException
-            ("key must have length >= 1");
+        if (key.length() == 0) {
+            throw new IllegalArgumentException(
+                "key must have length >= 1");
+        }
         Node<Value> x = get(root, key, 0);
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         return x.val;
     }
     /**
      * { function_description }.
      *
-     * @param      x     { parameter_description }.
+     * @param      x     { parameter_description }
      * @param      key   The key
-     * @param      d     { parameter_description }.
+     * @param      d     { parameter_description }
      *
-     * @return     { description_of_the_return_value }.
+     * @return     { description_of_the_return_value }
      */
     private Node<Value> get(final Node<Value> x,
         final String key, final int d) {
-        if (x == null) return null;
-        if (key.length() == 0) throw new
-            IllegalArgumentException("key must have length >= 1");
+        if (x == null) {
+            return null;
+        }
+        if (key.length() == 0) {
+            throw new IllegalArgumentException("key must have length >= 1");
+        }
         char c = key.charAt(d);
-        if      (c < x.c)              return get(x.left,  key, d);
-        else if (c > x.c)              return get(x.right, key, d);
-        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
-        else                           return x;
+        if      (c < x.c) {
+            return get(x.left,  key, d);
+        } else if (c > x.c) {
+            return get(x.right, key, d);
+        } else if (d < key.length() - 1) {
+            return get(x.mid,   key, d + 1);
+        } else {
+            return x;
+        }
     }
 
     /**
-     * Inserts the key-value pair into the symbol.
-     * table, overwriting the old value.
-     * with the new value if the key is already in the symbol table.
-     * If the value is {@code null}, this effectively deletes.
-     * the key from the symbol table.
-     * @param key the key.
-     * @param val the value.
-     * @throws IllegalArgumentException if {@code key} is {@code null}.
+     * Inserts the key-value pair into the
+     * symbol table, overwriting the old
+     * value with the new value if the key
+     * is already in the symbol table. If
+     * the value is {@code null}, this
+     * effectivelydeletes the key from the
+     * symbol table.
+     *
+     * @param      key   the key
+     * @param      val   the value
+     * @throws     IllegalArgumentException
+     * if {@code key} is {@code null}
      */
     public void put(final String key, final Value val) {
         if (key == null) {
-            throw new IllegalArgumentException("calls put() with null key");
+            throw new IllegalArgumentException(
+                "calls put() with null key");
         }
-        if (!contains(key)) n++;
+        if (!contains(key)) {
+            n++;
+        }
         root = put(root, key, val, 0);
     }
+
     /**
      * { function_description }.
      *
-     * @param      x     { parameter_description }.
-     * @param      key   The key.
-     * @param      val   The value.
-     * @param      d     { parameter_description }.
+     * @param      a     { parameter_description }
+     * @param      key   The key
+     * @param      val   The value
+     * @param      d     { parameter_description }
      *
-     * @return     { description_of_the_return_value }.
+     * @return     { description_of_the_return_value }
      */
-
-    private Node<Value> put(Node<Value> x, final String key, final Value val, int d) {
+    private Node<Value> put(final Node<Value> a,
+        final String key, final Value val, final int d) {
+        Node<Value> x = a;
         char c = key.charAt(d);
         if (x == null) {
             x = new Node<Value>();
             x.c = c;
         }
-        if      (c < x.c)               x.left  = put(x.left,  key, val, d);
-        else if (c > x.c)               x.right = put(x.right, key, val, d);
-        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
-        else                            x.val   = val;
+        if (c < x.c) {
+            x.left  = put(x.left,  key, val, d);
+        } else if (c > x.c) {
+            x.right = put(x.right, key, val, d);
+        } else if (d < key.length() - 1) {
+            x.mid   = put(x.mid,   key, val, d + 1);
+        } else {
+            x.val   = val;
+        }
         return x;
     }
 
     /**
-     * Returns the string in the symbol table that is the longest prefix of {@code query},
-     * or {@code null}, if no such string.
-     * @param query the query string
-     * @return the string in the symbol table that is the longest prefix of {@code query},
-     *     or {@code null} if no such string
-     * @throws IllegalArgumentException if {@code query} is {@code null}
+     * Returns the string in the symbol table that
+     * is the longest prefix of
+     * {@code query}, or {@code null}, if no such string.
+     *
+     * @param      query  the query string
+     *
+     * @return     the string in the symbol table
+     * that is the longest prefix of
+     *             {@code query}, or {@code null}
+     *             if no such string
+     * @throws     IllegalArgumentException
+     * if {@code query} is {@code null}
      */
     public String longestPrefixOf(final String query) {
         if (query == null) {
-            throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
+            throw new IllegalArgumentException(
+                "calls longestPrefixOf() with null argument");
         }
-        if (query.length() == 0) return null;
+        if (query.length() == 0) {
+            return null;
+        }
         int length = 0;
         Node<Value> x = root;
         int i = 0;
         while (x != null && i < query.length()) {
             char c = query.charAt(i);
-            if      (c < x.c) x = x.left;
-            else if (c > x.c) x = x.right;
-            else {
+            if      (c < x.c) {
+                x = x.left;
+            } else if (c > x.c) {
+                x = x.right;
+            } else {
                 i++;
-                if (x.val != null) length = i;
+                if (x.val != null) {
+                    length = i;
+                }
                 x = x.mid;
             }
         }
@@ -168,10 +222,11 @@ public class TST<Value> {
     }
 
     /**
-     * Returns all keys in the symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     * @return all keys in the symbol table as an {@code Iterable}
+     * Returns all keys in the symbol table as an {@code Iterable}. To iterate
+     * over all of the keys in the symbol table named {@code st}, use the
+     * foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return     all keys in the symbol table as an {@code Iterable}
      */
     public Iterable<String> keys() {
         Queue<String> queue = new Queue<String>();
@@ -181,48 +236,61 @@ public class TST<Value> {
 
     /**
      * Returns all of the keys in the set that start with {@code prefix}.
-     * @param prefix the prefix
-     * @return all of the keys in the set that start with {@code prefix},
-     *     as an iterable
-     * @throws IllegalArgumentException if {@code prefix} is {@code null}
+     *
+     * @param      prefix  the prefix
+     *
+     * @return     all of the keys in the set that start with {@code prefix}, as
+     *             an iterable
+     * @throws     IllegalArgumentException  if {@code prefix} is {@code null}
      */
     public Iterable<String> keysWithPrefix(final String prefix) {
         if (prefix == null) {
-            throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+            throw new IllegalArgumentException(
+                "calls keysWithPrefix() with null argument");
         }
         Queue<String> queue = new Queue<String>();
         Node<Value> x = get(root, prefix, 0);
-        if (x == null) return queue;
-        if (x.val != null) queue.enqueue(prefix);
+        if (x == null) {
+            return queue;
+        }
+        if (x.val != null) {
+            queue.enqueue(prefix);
+        }
         collect(x.mid, new StringBuilder(prefix), queue);
         return queue;
     }
-
     /**
      * { function_description }.
      *
-     * @param      x       { parameter_description }.
-     * @param      prefix  The prefix.
-     * @param      queue   The queue.
+     * @param      x       { parameter_description }
+     * @param      prefix  The prefix
+     * @param      queue   The queue
      */
     private void collect(final Node<Value> x,
-        final StringBuilder prefix,
-        final Queue<String> queue) {
-        if (x == null) return;
+        final StringBuilder prefix, final Queue<String> queue) {
+        if (x == null) {
+            return;
+        }
         collect(x.left,  prefix, queue);
-        if (x.val != null) queue.enqueue(prefix.toString() + x.c);
+        if (x.val != null) {
+            queue.enqueue(prefix.toString() + x.c);
+        }
         collect(x.mid,   prefix.append(x.c), queue);
         prefix.deleteCharAt(prefix.length() - 1);
         collect(x.right, prefix, queue);
     }
-
-
     /**
-     * Returns all of the keys in the symbol table that match {@code pattern},
+     * Returns all of the keys in the symbol
+     * table that match {@code pattern},
      * where . symbol is treated as a wildcard character.
-     * @param pattern the pattern.
-     * @return all of the keys in the symbol table that match {@code pattern},
-     *     as an iterable, where . is treated as a wildcard character.
+     *
+     * @param      pattern  the pattern
+     *
+     * @return     all of the keys in the
+     * symbol table that match {@code
+     *             pattern}, as an iterable,
+     *             where . is treated as a wildcard
+     *             character.
      */
     public Iterable<String> keysThatMatch(final String pattern) {
         Queue<String> queue = new Queue<String>();
@@ -230,7 +298,7 @@ public class TST<Value> {
         return queue;
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @param      x        { parameter_description }
      * @param      prefix   The prefix
@@ -238,19 +306,27 @@ public class TST<Value> {
      * @param      pattern  The pattern
      * @param      queue    The queue
      */
-
-    private void collect(final Node<Value> x, final StringBuilder prefix, final int i, final String pattern, final Queue<String> queue) {
-        if (x == null) return;
+    private void collect(final Node<Value> x,
+        final StringBuilder prefix, final int i,
+        final String pattern, final Queue<String> queue) {
+        if (x == null) {
+            return;
+        }
         char c = pattern.charAt(i);
-        if (c == '.' || c < x.c) collect(x.left, prefix, i, pattern, queue);
+        if (c == '.' || c < x.c) {
+            collect(x.left, prefix, i, pattern, queue);
+        }
         if (c == '.' || c == x.c) {
-            if (i == pattern.length() - 1 && x.val != null) queue.enqueue(prefix.toString() + x.c);
+            if (i == pattern.length() - 1 && x.val != null) {
+                queue.enqueue(prefix.toString() + x.c);
+            }
             if (i < pattern.length() - 1) {
-                collect(x.mid, prefix.append(x.c), i+1, pattern, queue);
+                collect(x.mid, prefix.append(x.c), i + 1, pattern, queue);
                 prefix.deleteCharAt(prefix.length() - 1);
             }
         }
-        if (c == '.' || c > x.c) collect(x.right, prefix, i, pattern, queue);
+        if (c == '.' || c > x.c) {
+            collect(x.right, prefix, i, pattern, queue);
+        }
     }
 }
-
